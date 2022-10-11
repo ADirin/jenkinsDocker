3715bdc166a904b4770b74cf16dc763ac2acf29f
Amir Dirin <amir.dirin@yahoo.com>
2022-10-11 19:49:04 +0300

    Third commit

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Hi again");
+        for (int i= 0; i<10; i++) {
+            System.out.println(" " +i);
+        }
     }
 }
